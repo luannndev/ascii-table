bb30ed0c177e3588ad0cf2a349fc9ac21c632d7e
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-20 21:15:06 +0200

    Uploaded project

--- ascii-table/Program.cs
+++ ascii-table/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using ascii_table;
 
-Console.WriteLine("Hello, World!");
+namespace ASCII_Table;
+
+internal class Program
+{
+    private static void Main()
+    {
+        var Lines = new List<string[]>
+        {
+            new[] { " serverid ", " roleid ", " permissions " },
+            new[] { " 829364130317336666 ", " 848176387029991434", " 8749823749284 " }
+        };
+        
+        Console.WriteLine(new ASCIITable(Lines));
+    }
+}
